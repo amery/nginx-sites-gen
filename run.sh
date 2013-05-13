92aa4ac87701272aaef5d17a9871bc136ba557f7
Alejandro Mery <amery@geeks.cl>
2013-05-13 15:54:38 +0000

    run.sh: add master script to run over the whole tree

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+die() {
+	echo "$@" >&1
+	exit 1
+}
+
+set -e
+ARG0=$(readlink -f "$0")
+[ -x "$ARG0" ] || die "$0: not found"
+U="${ARG0%/*}/update.sh"
+S="$PWD/sites.conf"
+F=
+rm -f "$S~"
+for d in */*/; do
+	[ -d "$d" ] || continue
+	d="${d%/}"
+	c="$PWD/$d.conf"
+	[ -s "$c" ] || touch "$c"
+
+	l="include \"$c\";"
+	if ! grep -q "^$l\$" "$S"; then
+		echo "$l" >> "$S"
+	fi
+	echo "$l" >> "$S~"
+
+	$U "$d/"
+done
+
+echo "====="
+mv "$S~" "$S"
+nginx -t && nginx -s reload
