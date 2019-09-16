ad9129d19cb9f2a5683db6e16f4859738c17b7ca
Alejandro Mery <amery@geeks.cl>
2019-09-16 23:23:54 +0200

    get_wildcard: add support for google and cloudflare backed domains
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- get_wildcard.sh
+++ get_wildcard.sh
@@ -2,11 +2,42 @@
 
 set -eu
 
+case "${1:-}" in
+-c|--cloudflare)
+	MODE=cloudflare
+	shift
+	;;
+-g|--google)
+	MODE=google
+	shift
+	;;
+-*)
+	echo "$1: mode not supported" >&2
+	exit 1
+	;;
+*)
+	MODE=manual
+	;;
+esac
+
+case "$MODE" in
+cloudflare)
+	MODE_OPTS="--dns-$MODE --dns-$MODE-credentials $HOME/.secrets/$MODE.ini"
+	;;
+google)
+	# https://certbot-dns-google.readthedocs.io/en/stable/
+	MODE_OPTS="--dns-$MODE --dns-$MODE-credentials $HOME/.secrets/$MODE.json"
+	;;
+manual)
+	MODE_OPTS="--$MODE --$MODE-public-ip-logging-ok"
+	;;
+esac
+
 for D; do
 	certbot certonly \
 		${M:+-m "$M"} --agree-tos \
 		--server https://acme-v02.api.letsencrypt.org/directory \
 		--preferred-challenges dns-01 \
-		--manual --manual-public-ip-logging-ok \
+		${MODE_OPTS} \
 		-d "$D" -d "*.$D"
 done
